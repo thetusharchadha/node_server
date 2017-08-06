d316514ac7c8c016bde711130ea7232a14a83ea6
Tushar Chadha <thetusharchadha@gmail.com>
2017-08-06 20:52:31 +0530

    added start

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname+"/views/partials")
 app.use(express.static(__dirname+"/public"))
@@ -23,6 +24,6 @@ app.get("/bad",(req,res)=>{
 });
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
   console.log("port 3000")
 });
